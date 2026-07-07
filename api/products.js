1bcbb185d04a22c3d3526e713648100a6445bc56
ezebuikay-ctrl <your-email@example.com>
2026-07-07 09:47:07 -0700

    Update DB connection to handle sslmode=disable for local/private host

--- api/products.js
+++ api/products.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
+  ssl: process.env.DATABASE_URL.includes('localhost') || process.env.DATABASE_URL.includes('helium') ? false : {
     rejectUnauthorized: false
   }
 });
